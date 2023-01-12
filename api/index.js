bbc6ed744dd233e09b96adfedf0abba9cc472235
Leandro Freire <fotografoleandro81@gmail.com>
2023-01-12 11:51:45 -0300

    correccion de puerto 2

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(port, () => {
     console.log('%s listening at: ', port); // eslint-disable-line no-console
   });
 });
