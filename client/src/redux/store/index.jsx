cad43e8dac471b61e02ca3275c56927a757cc8c4
Leandro Freire <fotografoleandro81@gmail.com>
2023-01-12 13:39:44 -0300

    store

--- client/src/redux/store/index.jsx
+++ client/src/redux/store/index.jsx
@@ -1,8 +1,21 @@
-import rootReducer from "../reducer";
+/* import rootReducer from "../reducer";
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 import  { composeWithDevTools } from "redux-devtools-extension"
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store; */
+
+import {createStore, applyMiddleware, compose} from "redux";
+import rootReducer from "../reducer/index";
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
