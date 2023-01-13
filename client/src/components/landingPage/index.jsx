658327a7414544202b10d22d74fa5a99c028313a
Leandro Freire <fotografoleandro81@gmail.com>
2023-01-13 14:13:03 -0300

    1

--- client/src/components/landingPage/index.jsx
+++ client/src/components/landingPage/index.jsx
@@ -4,16 +4,12 @@ import letras from '../../img/imagen3.jpg'
 
 
 export default function LandingPage(){
-    const handleClick = ()=>{
-        window.open('https://www.linkedin.com/in/freireleandro/', '_blank')
-        }
     return(
         <div className={style.landing}>
             <div className={style.text}>
                 <a className={style.text3} href="https://www.linkedin.com/in/freireleandro/">https://www.linkedin.com/in/freireleandro/</a>
             </div>
-                 <img className={style.video} src={letras} alt='not found'/>  *
-             {/*    <img className={style.fondo} src={fondo} alt='not found'/>  */}
+                 <img className={style.video} src={letras} alt='not found'/>  
             <Link to='/home'>
                 <button className={style.button} >Entrar</button>
             </Link>
